01f61ef05b29ca3549996d5c7065aea82fb94d18
erika.perciliano <erikaperciliano.developer@gmail.com>
2023-07-28 01:47:18 +0100

    add adjustment of styles

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -104,7 +104,7 @@ export const StartCountDownButton = styled.button`
     background: ${(props) => props.theme['green-500']};
     color: ${(props) => props.theme['gray-100']};
 
-    &:desabled {
+    &:disabled {
         opacity: 0.7;
         cursor: not-allowed;
     }
