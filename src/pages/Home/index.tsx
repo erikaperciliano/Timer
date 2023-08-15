b38532ea5a758134643521e4c6de3814f6e0195c
Erika Perciliano <erikaperciliano.developer@gmail.com>
2023-08-15 17:07:17 +0100

    change tab title

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -36,11 +36,18 @@ export const Home = () => {
     const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
     useEffect(() => {
+        let interval: number;
+
         if(activeCycle){
             setInterval(() => {
                 setAmountSecondPassed(differenceInSeconds(new Date(), activeCycle.startDate ))
             },1000)
         }
+
+        return () => {
+            clearInterval(interval)
+        }
+
     }, [activeCycle])
 
     const handleCreateNewCycle = (data: newCycleFormData) => {
@@ -54,6 +61,8 @@ export const Home = () => {
 
         setCycles((state) => [...state, newCycle])
         setActiveCycleId(id)
+        setAmountSecondPassed(0)
+
         reset();
     }
 
@@ -66,6 +75,12 @@ export const Home = () => {
 
     const minutes = String(minutesAmount).padStart(2, '0')
     const seconds = String(secondsAmount).padStart(2, '0') 
+
+   useEffect(() => {
+    if(activeCycle){
+        document.title = `${minutes} : ${seconds}`
+    }
+   }, [minutes, seconds, activeCycle])
 
     console.log(activeCycle)
 
