092311f0eed727ed66e70ee3bc50ba6c5d2b31ab
Erika Perciliano <erikaperciliano.developer@gmail.com>
2023-09-21 20:52:51 +0100

    add the Context API to the project

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 import { Router } from './Router'
 import { BrowserRouter } from 'react-router-dom'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 
 export const App = () => {
+
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-       <Router/>
+        <CyclesContextProvider>
+          <Router/>
+        </CyclesContextProvider>
       </BrowserRouter>
 
       <GlobalStyle />
