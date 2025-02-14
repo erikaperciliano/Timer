ea276be6a4d532b394a27ce030389aa3d00e7492
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-02-14 18:03:30 +0000

    structuring the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
-    <h1>Hello Wolrd</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="success"/>
+      <Button variant="danger"/>
+
+      <GlobalStyle/>
+    </ThemeProvider>
   )
 }
